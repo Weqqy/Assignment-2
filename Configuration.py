50e212c35a95fdb995cae504c37b09bb74a22d24
root <root@oa-s144-03.fast.sheridanc.on.ca>
2024-02-20 15:53:28 -0500

    new

--- Configuration.py
+++ Configuration.py
@@ -2,19 +2,32 @@ from netmiko import ConnectHandler
 from getpass import getpass
 
 # Define device parameters
+password = getpass()
 device = {
     'device_type': 'cisco_ios',
-    'host': '192.168.1.1',
-    'username': 'cameron',
-    'password': 'telecomS144',
+    'host': '192.168.123.2',
+    'username': 'admin',
+    'password': password,
     # 'port': 22,  # Change if using a different SSH port
-    # 'secret': 'enable_password',  # Uncomment and provide enable password if necessary
+    #'secret': 'enable_password',  # Uncomment and provide enable password if necessary
 }
-
+# Config device
+config = {
+    'interface lo0',
+    'ip address 10.1.1.1 255.255.255.255',
+    'exit'
+    'end',
+}
 # Connect to the device
 try:
     net_connect = ConnectHandler(**device)
     print("Connected successfully to the device.")
+
+    # Send configuration commands to the device
+    output = net_connect.send_config_set(config)
+    print("Configuration commands sent successfully.")
+    print("Output:")
+    print(output)
 
     # Example: Sending a command and printing the output
     output = net_connect.send_command('show ip interface brief')
